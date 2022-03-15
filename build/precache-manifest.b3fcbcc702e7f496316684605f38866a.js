a0db7e942a05f817a730d546cea0c263b07d1e11
JontyEllis12 <82895197+JontyEllis12@users.noreply.github.com>
2022-03-15 16:28:16 +0000

    Build

---
+++ build/precache-manifest.b3fcbcc702e7f496316684605f38866a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a6ebd0e39abb2d2d312ec4b652a44b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11009bcee39c079bec1b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f99f03357a96f6cabac7",
+    "url": "/static/js/2.e95693b6.chunk.js"
+  },
+  {
+    "revision": "cfcf3bdf61240f939f4f92d65c16f3bd",
+    "url": "/static/js/2.e95693b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4c0e1e479d1f00ead4",
+    "url": "/static/js/3.88726e74.chunk.js"
+  },
+  {
+    "revision": "11009bcee39c079bec1b",
+    "url": "/static/js/main.db5ac4b7.chunk.js"
+  },
+  {
+    "revision": "91fb3aa7cd53a9781bd5",
+    "url": "/static/js/runtime-main.e215ec59.js"
+  },
+  {
+    "revision": "b074de0eb3875a88986c41042dcc861a",
+    "url": "/static/media/itsonlyhousie.b074de0e.png"
+  },
+  {
+    "revision": "ddc24ee471f234969196a58f11fa1b83",
+    "url": "/static/media/jontysfridayquiz.ddc24ee4.png"
+  }
+]);
