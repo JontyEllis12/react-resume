98b0ba814ebc6622bf3df92932a73fabd321220c
JontyEllis12 <82895197+JontyEllis12@users.noreply.github.com>
2022-03-15 16:51:04 +0000

    Updates

---
+++ precache-manifest.e38c8be83a584f48a0cb240e48357791.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27838a672a53513f0df43058aecb602e",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "11009bcee39c079bec1b",
+    "url": "/react-resume/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "47ce9b25b0503a3da00a",
+    "url": "/react-resume/static/js/2.07846904.chunk.js"
+  },
+  {
+    "revision": "cfcf3bdf61240f939f4f92d65c16f3bd",
+    "url": "/react-resume/static/js/2.07846904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4c0e1e479d1f00ead4",
+    "url": "/react-resume/static/js/3.88726e74.chunk.js"
+  },
+  {
+    "revision": "11009bcee39c079bec1b",
+    "url": "/react-resume/static/js/main.db5ac4b7.chunk.js"
+  },
+  {
+    "revision": "02df6b09b22ad960a2ec",
+    "url": "/react-resume/static/js/runtime-main.38c8e8ee.js"
+  },
+  {
+    "revision": "b074de0eb3875a88986c41042dcc861a",
+    "url": "/react-resume/static/media/itsonlyhousie.b074de0e.png"
+  },
+  {
+    "revision": "ddc24ee471f234969196a58f11fa1b83",
+    "url": "/react-resume/static/media/jontysfridayquiz.ddc24ee4.png"
+  }
+]);
